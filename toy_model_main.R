f0f976ee20aa92405416d51b9293bf1f346e304e
Kaitlyn Martinez <kaitlynm@pn2003006.lanl.gov>
2021-05-18 12:33:53 -0600

    Intial File Directory set up

---
+++ toy_model_main.R
@@ -0,0 +1,15 @@
+#### Toy model
+
+### Inputs 
+# Wetness
+# Temperature
+# Lat/Long
+# start/stop date
+# MHI
+
+### Outputs
+# Mosquito Population csv for each hydro pop
+
+### Libraries 
+library(yaml) # to read in yaml file
+
