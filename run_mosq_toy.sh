c8626507bae6e3b9e59727614a36490b8a7abdde
Nidhi Parikh <nidhip@lanl.gov>
2021-08-10 13:25:50 -0600

    Changes to take config file as an argument

--- run_mosq_toy.sh
+++ run_mosq_toy.sh
@@ -1,1 +1,13 @@
-Rscript toy_model_main.R
+#!/bin/sh
+
+# Takes one argument:
+# Path to config file
+
+if [ "$#" -lt 1 ] || ! [ -f "$1" ] ; then
+    echo "ERROR!! Must specify configuration file path as an argument."
+fi
+
+
+CONFIG_FILE=$1
+
+Rscript toy_model_main.R $CONFIG_FILE
